516d9a0093015e5f02e94df9302ea78fe4299782
Nugroho Satriyanto <massatriya@gmail.com>
2024-03-02 10:47:09 +0700

    add pow

---
+++ Pow(x, n)/solution.py
@@ -0,0 +1,30 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if (n == 0):
+            return 1
+        if (n == 1):
+            return x
+        if (n == -1):
+            return 1/x
+        else:
+            return self.myPowMemo(x, n, {})
+
+    def myPowMemo(self, x: float, n: int, memo = {}) -> float:
+        if n in memo:
+            return memo[n]
+        if (n == 0):
+            return 1
+        if (n == 1):
+            return x
+        if (n == -1):
+            return 1/x
+        else:
+            n1 = n // 2
+            n2 = n - n1
+            print(n1, n2)
+            pn1 = self.myPowMemo(x, n1, memo)
+            memo[n1] = pn1 
+            pn2 = self.myPowMemo(x, n2, memo)
+            memo[n2] = pn2
+            return pn1 * pn2
+
