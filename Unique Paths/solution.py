7dab5a37ae2538b8597a6fd7bf728ba2a8f3c037
nugroho.satriyanto <nugroho.satriyanto@traveloka.com>
2024-04-08 20:15:31 +0700

    add comments

--- Unique Paths/solution.py
+++ Unique Paths/solution.py
@@ -6,10 +6,13 @@ class Solution:
             return 1
         if m is 2 and n is 2:
             return 2
+        # get value from memo if it exists
+        # uniquePaths(m, n) = uniquePaths(n,m)
         if tuple([m,n]) in memo:
             return memo[tuple([m,n])]
         if tuple([n,m]) in memo:
             return memo[tuple([n,m])]
+        # uniquePaths(m, n) = uniquePaths(m-1, n) + uniquePaths(m, n-1)
         right_path = self.uniquePaths(m, n - 1)
         memo[tuple([m, n - 1])] = right_path
         down_path = self.uniquePaths(m-1, n)
