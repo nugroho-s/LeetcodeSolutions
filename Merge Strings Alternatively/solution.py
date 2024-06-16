0b46a65ba5d80756128707fd270ccc5416b46c28
nugroho.satriyanto <nugroho.satriyanto@traveloka.com>
2024-06-16 20:49:47 +0700

    add merge strings alternatively

---
+++ Merge Strings Alternatively/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        min_len = min(len(word1), len(word2))
+        result = ""
+        for i in range(min_len):
+            result += word1[i]
+            result += word2[i]
+        result += word1[min_len:]
+        result += word2[min_len:]
+        return result
